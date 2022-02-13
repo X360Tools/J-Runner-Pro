02661e690ec47d3cfe426a2e40f24b12553a76bb
balika011 <balika011@gmail.com>
2022-02-13 14:20:03 -0800

    Upgrade to VS2022; remove Build.txt

--- J-Runner/Classes/variables.cs
+++ J-Runner/Classes/variables.cs
@@ -64,13 +64,36 @@ namespace JRunner
 
     class variables
     {
+        private static DateTime GetLinkerTime(Assembly assembly, TimeZoneInfo target = null)
+        {
+            var filePath = assembly.Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+
+            var buffer = new byte[2048];
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                stream.Read(buffer, 0, 2048);
+
+            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+
+            return localTime;
+        }
+
         public enum JR_MODE
         {
             MODEJR,
             MODEFW
         }
         public static string version = "3.1.0d Beta";
-        public static string build = "3103." + DateTime.ParseExact(Properties.Resources.Build.Trim(), "MM-dd-yyyy HH:mm:ss", null).ToString("yyMMdd.HHmm");
+        public static string build = "3103." + GetLinkerTime(Assembly.GetExecutingAssembly()).ToString("yyMMdd.HHmm");
         public const string staticversion = "3.1.0.3";
         public static int revision = 15;
         public static bool iswriting;
